27dcf6483e57ebb5e6b88bdc6241181141b9ce16
baokiin <baokiinkk@gmail.com>
2020-03-17 16:01:27 +0700

    test7

--- app/src/main/java/com/example/applambaikiemtra/ui/cauhoi/AdapterRecycleView.kt
+++ app/src/main/java/com/example/applambaikiemtra/ui/cauhoi/AdapterRecycleView.kt
@@ -13,20 +13,42 @@ import kotlinx.android.synthetic.main.custom_bai_thi.view.*
 import java.util.*
 
 
-class ViewHodel(v: View):RecyclerView.ViewHolder(v)
-{
 
-
-    val txtCau=v.txtCau
-    val txtCauHoi=v.txtCauHoi
-    val btnA=v.A
-    val btnB=v.B
-    val btnC=v.C
-    val btnD=v.D
-}
-class AdapterRecycleView(val list:MutableLiveData<MutableList<MutableMap<String,String>> >) :RecyclerView.Adapter<ViewHodel>() {
-
+class AdapterRecycleView(val list:MutableLiveData<MutableList<MutableMap<String,String>> >) :RecyclerView.Adapter<AdapterRecycleView.ViewHodel>() {
     var boolean: Boolean=false
+    val listLuuVitri:MutableList<Int> = mutableListOf()
+    init {
+        for (i in 0..(list.value?.size!!))
+            listLuuVitri.add(-1)
+    }
+
+   inner class ViewHodel(v: View):RecyclerView.ViewHolder(v),View.OnClickListener
+    {
+        override fun onClick(v: View?) {
+           when(v?.id)
+           {
+               btnA.id->listLuuVitri[adapterPosition]=1
+               btnB.id->listLuuVitri[adapterPosition]=2
+               btnC.id->listLuuVitri[adapterPosition]=3
+               btnD.id->listLuuVitri[adapterPosition]=4
+           }
+        }
+
+        val txtCau=v.txtCau
+        val txtCauHoi=v.txtCauHoi
+        val btnA=v.A
+        val btnB=v.B
+        val btnC=v.C
+        val btnD=v.D
+        init {
+            btnA.setOnClickListener(this)
+            btnB.setOnClickListener(this)
+            btnC.setOnClickListener(this)
+            btnD.setOnClickListener(this)
+        }
+
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHodel {
         val view =LayoutInflater.from(parent.context).inflate( R.layout.custom_bai_thi,parent,false)
         val viewHodel=ViewHodel(view)
@@ -54,18 +76,21 @@ class AdapterRecycleView(val list:MutableLiveData<MutableList<MutableMap<String,
             var vitriChon:Int=-1
             var vitriDapAn:Int=-1
             var dapan: String? = list.value?.get(position)?.get("Đáp án")
-            if(holder.btnA.isChecked == true) vitriChon=1
-            else if(holder.btnB.isChecked == true) vitriChon=2
-            else if(holder.btnC.isChecked == true) vitriChon=3
-            else if(holder.btnD.isChecked == true) vitriChon=4
-            else vitriChon=-1
 
             if(A == dapan) vitriDapAn=1
             else if(B == dapan) vitriDapAn=2
             else if(C == dapan) vitriDapAn=3
             else  vitriDapAn=4
 
-            check(holder.btnA,holder.btnB,holder.btnC,holder.btnD,vitriDapAn,vitriChon)
+            if(listLuuVitri[position] == 1)
+                holder.btnA.isChecked=true
+            else if(listLuuVitri[position] == 2)
+                holder.btnB.isChecked=true
+            else if(listLuuVitri[position]== 3)
+                holder.btnC.isChecked=true
+            else
+                holder.btnD.isChecked=true
+            check(holder.btnA,holder.btnB,holder.btnC,holder.btnD,vitriDapAn,listLuuVitri[position])
         }
 
     }
@@ -80,10 +105,22 @@ class AdapterRecycleView(val list:MutableLiveData<MutableList<MutableMap<String,
     }
     fun toXanh(vitri:Int,btnA: RadioButton,btnB: RadioButton,btnC: RadioButton,btcD: RadioButton)
     {
-        if(vitri == 1) btnA.setBackgroundResource(R.drawable.checked)
-        else if(vitri == 2) btnB.setBackgroundResource(R.drawable.checked)
-        else if(vitri == 3) btnC.setBackgroundResource(R.drawable.checked)
-        else  btcD.setBackgroundResource(R.drawable.checked)
+        if(vitri == 1)
+        {
+            btnA.setBackgroundResource(R.drawable.checked)
+
+        }
+        else if(vitri == 2)
+        {
+            btnB.setBackgroundResource(R.drawable.checked)
+        }
+        else if(vitri == 3){
+            btnC.setBackgroundResource(R.drawable.checked)
+        }
+        else
+        {
+            btcD.setBackgroundResource(R.drawable.checked)
+        }
     }
     fun toDo(vitri:Int,btnA: RadioButton,btnB: RadioButton,btnC: RadioButton,btcD: RadioButton)
     {
@@ -95,7 +132,7 @@ class AdapterRecycleView(val list:MutableLiveData<MutableList<MutableMap<String,
     fun check(btnA:RadioButton,btnB:RadioButton,btnC:RadioButton,btcD: RadioButton,dapan:Int,luachon:Int)
     {
         reset(btnA,btnB,btnC,btcD)
-        if(dapan == luachon || luachon ==-1)
+        if(dapan == luachon || luachon == -1)
             toXanh(dapan,btnA,btnB,btnC,btcD)
         else
         {
